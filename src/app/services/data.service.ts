372f3888f5b9922f602cd2a6acae8ebe9a85ebe4
JuliethSierra <127166230+JuliethSierra@users.noreply.github.com>
2023-05-29 12:53:37 -0500

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,8 +6,8 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class DataService {
 
-url = "http://10.4.72.222:30009/api/server"
-url1 = "http://10.4.72.222:30010/api1/server1"
+url = "http://192.168.20.146:30009/api/server"
+url1 = "http://192.168.20.146:30010/api1/server1"
 
   constructor(private http: HttpClient) {
     console.log("Servicio");
